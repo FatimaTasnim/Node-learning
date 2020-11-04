8152412705f3575603584da900e935f167c1cb01
FatimaTasnim <tasnim.roujat@gmail.com>
2020-11-04 19:35:22 +0600

    Chore: Callback Function Assignment - Writing a simple sum Async Function

--- Note-app/playground/4-callbacks.js
+++ Note-app/playground/4-callbacks.js
@@ -16,3 +16,15 @@ const geoCode = (adress, callback)=>{
 geoCode('dhaka', (data)=>{
     console.log(data);
 })
+
+const add = (a, b, callback)=>{
+    setTimeout(()=>{
+        sum = a + b;
+        callback(sum)
+    },2000)
+    
+}
+
+result = add(100, 200, (sum)=>{
+    console.log(sum)
+})
