2159430a0b7991af61f137f931feda2f3014b73f
FatimaTasnim <tasnim.roujat@gmail.com>
2021-03-10 15:40:29 +0600

    Add HTTP request with fetch

--- webServer/public/js/app.js
+++ webServer/public/js/app.js
@@ -1,1 +1,7 @@
 console.log('Client side javascript file is loaded!')
+
+fetch(url = "http://localhost:3000/weather?address=Dhaka").then((response)=>{
+    response.json().then((data)=>{
+        console.log(data)
+    })
+})
