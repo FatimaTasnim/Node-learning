2200bf7dcf4b43317c202f95ac8c84b007133f06
FatimaTasnim <tasnim.roujat@gmail.com>
2021-03-10 11:40:17 +0600

    Add Webserver with templates Closes #9

--- webServer/src/app.js
+++ webServer/src/app.js
@@ -1,20 +1,22 @@
 const path = require('path')
 const { response } = require('express')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express() // loading express app
 
 // serving folders for express
 const assetsPaths = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
-
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
 // static directory to serve
-app.use(express.static(assetsPaths))
+app.use(express.static(assetsPaths));
 
 
 // setting the template
-app.set('view engine', 'hbs') // 'view engine' format should be exact same
-app.set('views', viewsPath) // for views the default folder name is views, so if the folder name is smth other than views it should be added like this.
+app.set('view engine', 'hbs'); // 'view engine' format should be exact same
+app.set('views', viewsPath); // for views the default folder name is views, so if the folder name is smth other than views it should be added like this.
+hbs.registerPartial('header',partialPath); 
 
 /// check (express.org->api reference-> express application -> app.set() to explore more set options.
 
@@ -31,7 +33,7 @@ app.get('/about', (req, res)=>{
 
 app.get('/help', (req, res)=>{
     res.render('help', {
-        text: "hey! how can I help you?"
+        text: "hey! how can I help you???"
     })
 })
 
