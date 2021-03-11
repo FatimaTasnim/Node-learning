4c272246edc20debad7033f27b06a1188e996d0d
FatimaTasnim <tasnim.roujat@gmail.com>
2021-03-12 02:23:11 +0600

    Style the app

--- webServer/public/js/app.js
+++ webServer/public/js/app.js
@@ -13,9 +13,10 @@ const weatherForm       = document.querySelector('form')
 const selectLocation    = document.querySelector('input')
 weatherForm.addEventListener('submit', (event)=>{ 
     event.preventDefault(); // usually form refreshes with submit but prevent default prevents that mannter.
-    LodingStatus.textContent = "Loading . .. ...";
+    LodingStatus.textContent = "Loading...";
+    LodingStatus.style.color = "#B2F3E6";
     const Location = selectLocation.value;
-    
+
     LocationFetcher(Location, (data)=>{
         if(data.error){
             LodingStatus.style.color = 'red';
